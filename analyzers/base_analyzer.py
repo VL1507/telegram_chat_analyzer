f644dbd63ea0660f417bbefcfcad6590c12dd428
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-07-13 01:14:51 +1000

    Добавить инициализацию chat_data и метод для преобразования строк даты в объект datetime

--- analyzers/base_analyzer.py
+++ analyzers/base_analyzer.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 from typing import Any
 
 
 class BaseAnalyzer(ABC):
-    def __init__(self) -> None:
+    def __init__(self, chat_data: dict[str, Any]) -> None:
         super().__init__()
+        self.chat_data = chat_data
         self.stats: dict[str, Any] = dict()
 
     @abstractmethod
@@ -12,3 +14,6 @@ class BaseAnalyzer(ABC):
 
     def get_stats(self) -> dict[str, Any]:
         return self.stats
+
+    def _to_date(self, date_string: str, format_: str = "%Y-%m-%dT%H:%M:%S") -> datetime:
+        return datetime.strptime(date_string, format_)
