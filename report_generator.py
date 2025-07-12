1345515721d68843c5edeebaebe5688a361689fa
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-07-13 01:15:53 +1000

    Добавить поддержку пользовательского пути шаблона в класс ReportGenerator

--- report_generator.py
+++ report_generator.py
@@ -1,18 +1,53 @@
 from typing import Any
 
-from jinja2 import Environment, FileSystemLoader
+import jinja2
+
+# from jinja2 import Environment, FileSystemLoader
 
 
 class ReportGenerator:
-    def __init__(self, stats: dict[str, Any], output_path: str = "report.html") -> None:
+    def __init__(
+        self,
+        stats: dict[str, Any],
+        output_path: str = "report.html",
+        template_path: str | None = None,
+    ) -> None:
         self.stats = stats
         self.output_path = output_path
-        self.template_path = "report_template.html"
 
-    def generate_report(self) -> None:
-        file_loader = FileSystemLoader(".")
-        env = Environment(loader=file_loader)
-        template = env.get_template(self.template_path)
-        output = template.render(stats=self.stats)
-        with open(self.output_path, "w") as f:
-            f.write(output)
+        if template_path is None:
+            self.template_path = "report_template.html"
+        else:
+            self.template_path = template_path
+
+    def generate(self) -> None:
+        with open(self.template_path, "r", encoding="utf-8") as f:
+            template = jinja2.Template(f.read())
+        html = template.render(self.stats)
+
+        with open(self.output_path, "w", encoding="utf-8") as f:
+            f.write(html)
+
+    # def generate(self) -> None:
+    #     # file_loader = FileSystemLoader(".")
+    #     # env = Environment(loader=file_loader)
+    #     # env.globals.update(enumerate=enumerate)
+    #     # Инициализация Jinja2 с правильными настройками кодировки
+    #     env = Environment(
+    #         loader=FileSystemLoader('.'),
+    #         autoescape=True,
+    #         trim_blocks=True,
+    #         lstrip_blocks=True,
+    #         keep_trailing_newline=True
+    #     )
+
+    #     # Принудительная установка кодировки
+    #     # env.globals.update(encoding='utf-8')
+
+    #     template = env.get_template(self.template_path)
+    #     html = template.render(img_base64=self.stats.get("img_base64"))
+
+    #     print(self.stats)
+
+    #     with open(self.output_path, "w", encoding="utf-8") as f:
+    #         f.write(html)
