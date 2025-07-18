9447e849a5ac70bd8ab6335f410b050deebcc9c6
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-07-18 17:11:55 +1000

    Добавить методы для сохранения графиков и преобразования строковых дат в объекты datetime

--- analyzers/base_analyzer.py
+++ analyzers/base_analyzer.py
@@ -1,6 +1,10 @@
+import base64
 from abc import ABC, abstractmethod
 from datetime import datetime
+from io import BytesIO
 from typing import Any
+
+import matplotlib.pyplot as plt
 
 
 class BaseAnalyzer(ABC):
@@ -15,5 +19,13 @@ class BaseAnalyzer(ABC):
     def get_stats(self) -> dict[str, Any]:
         return self.stats
 
-    def _to_date(self, date_string: str, format_: str = "%Y-%m-%dT%H:%M:%S") -> datetime:
+    def _to_date(
+        self, date_string: str, format_: str = "%Y-%m-%dT%H:%M:%S"
+    ) -> datetime:
         return datetime.strptime(date_string, format_)
+
+    def _save_plot_to_base64(self):
+        buf = BytesIO()
+        plt.savefig(buf, format="png", bbox_inches="tight", dpi=100) # type: ignore
+        buf.seek(0)
+        return base64.b64encode(buf.read()).decode("utf-8")
